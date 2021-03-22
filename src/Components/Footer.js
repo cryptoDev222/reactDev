53bb7b82f1c6def905997a4295b8b3077c8abf5e
fullstackdev215 <genius.developer.63@gmail.com>
2021-03-22 13:32:59 -0700

    resume commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,21 +4,10 @@ class Footer extends Component {
   render() {
     if (!this.props.data) return null;
 
-    const networks = this.props.data.social.map(function(network) {
-      return (
-        <li key={network.name}>
-          <a href={network.url}>
-            <i className={network.className}></i>
-          </a>
-        </li>
-      );
-    });
-
     return (
       <footer>
         <div className="row">
           <div className="twelve columns">
-            <ul className="social-links">{networks}</ul>
 
             <ul className="copyright">
               <li>&copy; Copyright 2021 Nordic Giant</li>
